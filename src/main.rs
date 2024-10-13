689f92a2751f0e49855d08bbcdf6c7da23b874ae
Mattia Procopio <promat85@gmail.com>
2024-10-13 19:21:50 +0200

    Fix duration and return to vector

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,17 @@ struct NotificationMessage {
 fn main() {
     match ureq::get("http://astromatto.com:9000/notifications.json").call() {
         Ok(response) => {
-            if let Ok(message) = response.into_json::<NotificationMessage>() {
-                let _ = Notification::new()
-                    .appname("AstroArch news")
-                    .summary(message.summary.as_ref())
-                    .body(message.body.as_ref())
-                    .icon("astroarch")
-                    .timeout(5)
-                    .show()
-                    .unwrap();
+            if let Ok(messages) = response.into_json::<Vec<NotificationMessage>>() {
+                for message in messages.into_iter() {
+                    let _ = Notification::new()
+                        .appname("AstroArch news")
+                        .summary(message.summary.as_ref())
+                        .body(message.body.as_ref())
+                        .icon("astroarch")
+                        .timeout(5000)
+                        .show()
+                        .unwrap();
+                }
             }
         }
         Err(_) => (),
